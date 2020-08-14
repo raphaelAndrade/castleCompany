beb967cc6337462f3462da77a32651415988a6e6
Raphael Andrade <raphael.andrade@ibm.com>
2020-08-13 23:18:00 -0300

    Updates

---
+++ precache-manifest.e502e9a276eb447326fe7aa44674b41e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1129bda6bf91cd41fdf8e4efba19e39e",
+    "url": "/castleCompany/index.html"
+  },
+  {
+    "revision": "62f6d336ff4654d319de",
+    "url": "/castleCompany/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "ff31f84dcc1573ffe2f3",
+    "url": "/castleCompany/static/css/main.8d203c42.chunk.css"
+  },
+  {
+    "revision": "62f6d336ff4654d319de",
+    "url": "/castleCompany/static/js/2.6dc7e8dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/castleCompany/static/js/2.6dc7e8dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff31f84dcc1573ffe2f3",
+    "url": "/castleCompany/static/js/main.ccf756b1.chunk.js"
+  },
+  {
+    "revision": "d29968e152ed0f9c1f59",
+    "url": "/castleCompany/static/js/runtime-main.99f60103.js"
+  },
+  {
+    "revision": "3ad1ea8aa8fa30c279d2c45d7758956c",
+    "url": "/castleCompany/static/media/bg.3ad1ea8a.jpg"
+  }
+]);
